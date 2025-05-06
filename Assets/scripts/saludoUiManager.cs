3a45e3708680b209eb8015d2502b5f9723ef720f
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2025-05-06 12:16:56 -0300

    listo

---
+++ Assets/scripts/saludoUiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class saludoUiManager : MonoBehaviour
+{ public TextMeshProUGUI txtSaludo;
+    string nombreUsuario;
+    public TMP_InputField inputNombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = " ";
+    }
+
+    public void Saludar()
+    {
+        if (inputNombre.text == "")
+        {
+            txtSaludo.text = "usuario no ingresado";
+            return;
+        }
+        else
+        {
+            
+            txtSaludo.text = "hola" + inputNombre.text;
+        }
+    }
+}
